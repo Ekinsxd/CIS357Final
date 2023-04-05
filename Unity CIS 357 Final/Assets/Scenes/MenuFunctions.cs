c9028a196adbf2dfecaf30c1f4253b917dcbef8b
GVSU-Romandy-Vu <97295936+GVSU-Romandy-Vu@users.noreply.github.com>
2023-04-05 12:36:45 -0400

    Gave game name, some clean-up

--- Unity CIS 357 Final/Assets/Scenes/MenuFunctions.cs
+++ Unity CIS 357 Final/Assets/Scenes/MenuFunctions.cs
@@ -70,25 +70,20 @@ public class MenuFunctions: MonoBehaviour
 
     public void LoadInterstitialAd()
     {
-        // Clean up the old ad before loading a new one.
         if (interstitialAd != null)
         {
             interstitialAd.Destroy();
             interstitialAd = null;
         }
 
-        Debug.Log("Loading the interstitial ad.");
-
-        // create our request used to load the ad.
         var adRequest = new AdRequest.Builder()
                 .AddKeyword("unity-admob-sample")
                 .Build();
 
-        // send the request to load the ad.
+
         InterstitialAd.Load(_adUnitId, adRequest,
             (InterstitialAd ad, LoadAdError error) =>
             {
-                // if error is not null, the load request failed.
                 if (error != null || ad == null)
                 {
                     Debug.LogError("interstitial ad failed to load an ad " +
@@ -105,24 +100,15 @@ public class MenuFunctions: MonoBehaviour
 
     public void ShowAd()
     {
-        if (interstitialAd != null && interstitialAd.CanShowAd())
-        {
-            Debug.Log("Showing interstitial ad.");
-            interstitialAd.Show();
-        }
-        else
-        {
-            Debug.LogError("Interstitial ad is not ready yet.");
-        }
+        if (interstitialAd != null && interstitialAd.CanShowAd()){interstitialAd.Show();}
+        else{Debug.LogError("Interstitial ad is not ready yet.");}
     }
 
     private void RegisterReloadHandler(InterstitialAd ad)
     {
-        // Raised when the ad closed full screen content.
         ad.OnAdFullScreenContentClosed += () => {
             Debug.Log("Interstitial Ad full screen content closed.");
 
-            // Reload the ad so that we can show another as soon as possible.
             LoadInterstitialAd();
         };
         // Raised when the ad failed to open full screen content.
@@ -131,7 +117,6 @@ public class MenuFunctions: MonoBehaviour
             Debug.LogError("Interstitial ad failed to open full screen content " +
                            "with error : " + error);
 
-            // Reload the ad so that we can show another as soon as possible.
             LoadInterstitialAd();
         };
     }
